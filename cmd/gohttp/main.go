7d44510350cd33dda61ee4b38f7b33c65613fe0c
Sarat Chandra <saratchandra@outlook.com>
2015-09-09 22:15:43 +0530

    added log

--- cmd/gohttp/main.go
+++ cmd/gohttp/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 
-	"gohttp/Godeps/_workspace/src/github.com/gin-gonic/gin"
+	"gohttp/cmd/gohttp/Godeps/_workspace/src/github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
 	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 
 	// gin.H is a shortcut for map[string]interface{}
 	r.GET("/ip", func(c *gin.Context) {
