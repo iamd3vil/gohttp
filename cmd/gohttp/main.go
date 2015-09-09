e28c7d3115cfc69364f445d0753e1e0408a70dd0
Sarat Chandra <saratchandra@outlook.com>
2015-09-09 22:39:48 +0530

    Get the first value in X-Forwarded-Array

--- cmd/gohttp/main.go
+++ cmd/gohttp/main.go
@@ -19,7 +19,7 @@ func main() {
 
 	// gin.H is a shortcut for map[string]interface{}
 	r.GET("/ip", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"ip": c.Request.Header["X-Forwarded-For"]})
+		c.JSON(http.StatusOK, gin.H{"ip": c.Request.Header["X-Forwarded-For"][0]})
 	})
 	r.GET("/user-agent", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"user-agent": c.Request.UserAgent()})
